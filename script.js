a22fb874679ceea63c45f88bf0cc073346513b10
Youssefsaidd <157014888+Youssefsaidd@users.noreply.github.com>
2024-01-18 14:33:24 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function selectPlan(price) {
+    // You can implement logic here to store the selected plan
+    // and navigate to the payment page
+    window.location.href = "payment.html";
+}
+
+function processPayment() {
+    // You can implement payment processing logic here
+    alert("Thank you for your trust!");
+}
